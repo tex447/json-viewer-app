37dc43083c5a2affd79f1c7643b1b89b1aaf5a8a
Dave Lawson <david.lawson7@gmail.com>
2025-02-06 22:52:04 -0600

    Update vite.config.js with path alias

--- vite.config.js
+++ vite.config.js
@@ -2,18 +2,16 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3000
-  },
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src")
+      '@': path.resolve(__dirname, './src'),
     },
   },
-  build: {
-    outDir: 'public',
-    emptyOutDir: true
+  server: {
+    host: true,
+    port: 3000
   }
 })
